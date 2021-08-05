6ee8b4cb68c9499879ddc98643a0a02b72b0b7c0
Liquid <Liquid369@gmail.com>
2021-08-05 14:35:01 -0500

    DevFee to stake split

--- src/masternode-payments.cpp
+++ src/masternode-payments.cpp
@@ -350,13 +350,15 @@ void CMasternodePayments::FillBlockPayee(CMutableTransaction& txNew, const int n
                 } else if (i > 3) {
                     // special case, stake is split between (i-1) outputs
                     unsigned int outputs = i-1;
+                    CAmount devFeeSplit = nDevReward / outputs;
+                    CAmount devFeeRemainder = nDevReward - (devFeeSplit * outputs);
                     CAmount mnPaymentSplit = masternodePayment / outputs;
                     CAmount mnPaymentRemainder = masternodePayment - (mnPaymentSplit * outputs);
                     for (unsigned int j=1; j<=outputs; j++) {
-                        txNew.vout[j].nValue -= mnPaymentSplit;
+                        txNew.vout[j].nValue -= mnPaymentSplit + devFeeSplit;
                     }
                     // in case it's not an even division, take the last bit of dust from the last one
-                    txNew.vout[outputs].nValue -= mnPaymentRemainder;
+                    txNew.vout[outputs].nValue -= mnPaymentRemainder + devFeeRemainder;
                 }
             CTxDestination destination = DecodeDestination(Params().DevAddress());
             EncodeDestination(destination);
